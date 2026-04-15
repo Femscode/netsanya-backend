515120879d90a2be458cc7ffdc1c7d1484f6bae3
Pelumi <fasanyafemi@gmail.com>
2026-04-16 00:55:08 +0100

    first version

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => array_values(array_filter([
-        env('FRONTEND_ORIGIN', 'http://localhost:3000'),
+        // env('FRONTEND_ORIGIN', 'http://localhost:3000'),
         'https://netsanya.connectinskillz.com',
         'https://www.netsanya.connectinskillz.com',
     ])),
