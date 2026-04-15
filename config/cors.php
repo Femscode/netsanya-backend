e0731e9cc3fefb9a4904384a6b8fad9bfdfd7d6b
Pelumi <fasanyafemi@gmail.com>
2026-04-16 00:51:46 +0100

    first version

--- config/cors.php
+++ config/cors.php
@@ -5,11 +5,12 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => array_values(array_filter([
         env('FRONTEND_ORIGIN', 'http://localhost:3000'),
-        'http://localhost:3000',
+        'https://netsanya.connectinskillz.com',
+        'https://www.netsanya.connectinskillz.com',
     ])),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
